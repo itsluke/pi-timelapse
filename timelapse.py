621b06d7ce0a1a93af4a2643ea0f96a6b2063506
itsluke <hello@luke.ng>
2017-12-06 13:32:30 +0100

    gives feedback on export progress

--- timelapse.py
+++ timelapse.py
@@ -1,14 +1,40 @@
 from picamera import PiCamera
 import errno
 import os
+import re
 import sys
 import threading
-from datetime import datetime
+import Queue
+from datetime import datetime, timedelta
 from time import sleep
 import yaml
 
 config = yaml.safe_load(open(os.path.join(sys.path[0], "config.yml")))
+
+# defaults
+series_name = 'series'
+output_dir = sys.path[0]
+date_string = '%Y-%m-%d'
+time_string = '%H:%M:%S'
+
 image_number = 0
+total_images = 0
+mode = {}
+
+gif_location = ''
+mp4_location = ''
+
+# custom
+if 'series_name' in config:
+    series_name = config['series_name']
+if 'output_dir' in config:
+    output_dir = config['output_dir']
+if 'time_string' in config:
+    time_string = config['time_string']
+if 'date_string' in config:
+    date_string = config['date_string']
+
+FMT = '{} {}'.format(date_string, time_string)
 
 
 def create_timestamped_dir(dir):
@@ -53,13 +79,86 @@ def set_camera_options(camera):
     return camera
 
 
+def config_mode():
+    global image_number
+    global total_images
+    global mode
+    now = datetime.now()
+
+    # check if start and end time are present
+    if all(k in config for k in ("start_time", "end_time")):
+        now_date_str = now.strftime(date_string)
+
+        # setup daily attributes
+        mode['type'] = 'daily'
+        mode['start_time'] = datetime.strptime('{} {}'.format(
+            now_date_str, config['start_time']), FMT)
+        mode['end_time'] = datetime.strptime('{} {}'.format(
+            now_date_str, config['end_time']), FMT)
+
+        duration = mode['end_time'] - mode['start_time']
+        total_images = (duration/config['interval']).seconds
+
+        # set todays dir
+        mode['dir'] = os.path.join(
+            output_dir,
+            valid_filename(series_name),
+            valid_filename(now_date_str)
+        )
+
+        # TODO: need to account for start/end times
+        # that bridge midnight
+        if (now > mode['start_time']):
+            # set current image number if script restarts
+            # or is in the middle of daily run
+            image_number = ((now - mode['start_time']) / config['interval']).seconds
+    else:
+        total_images = config['total_images']
+        mode['type'] = 'Once'
+        dir_slug = valid_filename('{}-{}_{}'.format(
+                series_name, now.strftime(date_string), now.strftime(time_string)
+            ))
+        mode['dir'] = os.path.join(
+            output_dir,
+            dir_slug
+        )
+
+    pretty_print()
+
+
+def pretty_print():
+    print '------------', 'timelapse.py', '-------------'
+    if mode['type'] == 'Once':
+        print 'Mode: {} | Interval: {}s | Image Count: {}'.format(
+            mode['type'], config['interval'], total_images)
+
+        print 'Output Dir: {}/'.format(os.path.basename(mode['dir']))
+    else:
+        print 'Mode: {} | Interval: {}s | Daily Count: {}'.format(
+                mode['type'], config['interval'], total_images)
+        print 'timelapse from: {} to: {}'.format(
+            mode['start_time'].strftime(time_string),
+            mode['end_time'].strftime(time_string)
+        )
+        print 'Output Dir: {}/{}'.format(
+            series_name, os.path.basename(mode['dir'])
+        )
+
+    print '------------------------------------------'
+
+
+def valid_filename(s):
+    s = str(s).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', s)
+
+
 def capture_image():
     try:
         global image_number
 
         # Set a timer to take another picture at the proper interval after this
         # picture is taken.
-        if (image_number < (config['total_images'] - 1)):
+        if (image_number < (total_images - 1)):
             thread = threading.Timer(config['interval'], capture_image).start()
 
         # Start up the camera.
@@ -67,36 +166,152 @@ def capture_image():
         set_camera_options(camera)
 
         # Capture a picture.
-        camera.capture(dir + '/image{0:05d}.jpg'.format(image_number))
+        camera.capture(mode['dir'] + '/image{0:05d}.jpg'.format(image_number))
         camera.close()
 
-        if (image_number < (config['total_images'] - 1)):
+        print 'Taking image {}/{}'.format(image_number + 1, total_images)
+
+        if (image_number < (total_images - 1)):
             image_number += 1
         else:
-            print '\nTime-lapse capture complete!\n'
-            # TODO: This doesn't pop user into the except block below :(.
-            sys.exit()
+            finish_capture()
 
     except KeyboardInterrupt, SystemExit:
         print '\nTime-lapse capture cancelled.\n'
 
-# Create directory based on current timestamp.
-dir = os.path.join(
-    sys.path[0],
-    'series-' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-)
-create_timestamped_dir(dir)
 
-# Kick off the capture process.
-capture_image()
+def show_progress(action, action_args=(), msg='working', sec=0.5):
+    s = '.'
+    t = threading.Thread(target=action, args=action_args)
+    t.start()
+    sys.stdout.write( msg )
+    while t.is_alive():
+        sys.stdout.write( s )
+        sys.stdout.flush()
+        sleep(sec)
 
-# TODO: These may not get called after the end of the threading process...
-# Create an animated gif (Requires ImageMagick).
-if config['create_gif']:
-    print '\nCreating animated gif.\n'
-    os.system('convert -delay 10 -loop 0 ' + dir + '/image*.jpg ' + dir + '-timelapse.gif')  # noqa
 
-# Create a video (Requires avconv - which is basically ffmpeg).
-if config['create_video']:
-    print '\nCreating video.\n'
-    os.system('avconv -framerate 20 -i ' + dir + '/image%05d.jpg -vf format=yuv420p ' + dir + '/timelapse.mp4')  # noqa
+def make_gif():
+    global gif_location
+    # ImageMagick Setup, for more options see
+    # https://www.imagemagick.org/script/command-line-options.php
+    envs = ['MAGICK_THREAD_LIMIT=1', 'MAGICK_THROTTLE=50']
+    flags = ['-delay 10', '-loop 0' ]
+    output_dir = mode['dir']
+    g_suffix = '-timelapse.gif'
+    
+    if 'gif_flags' in config:
+        flags.insert(len(flags),config['gif_flags'])
+        
+    if 'gif_output_dir' in config:
+        output_dir = config['gif_output_dir']
+        
+    if 'gif_suffix' in config:
+        gif_suffix = config['gif_suffix']
+
+    g_input = mode['dir'] + '/image*.jpg'
+    gif_location = output_dir + g_suffix
+
+    # build command line string
+    cmd = [envs, ['convert'], flags, [g_input, gif_location]]
+    cmd = ' '.join(str(r) for v in cmd for r in v)
+
+    # run the system command
+    os.system(cmd)
+
+
+def make_video():
+    global mp4_location
+    mp4_location = mode['dir'] + '/timelapse.mp4'
+
+    os.system('avconv -framerate 20 -i ' + mode['dir'] + '/image%05d.jpg -vf format=yuv420p ' + mp4_location)
+
+
+def export_timelapse(type):
+    if type == 'gif':
+        export_func = make_gif
+        namespace = 'GIF:'
+        export_path = gif_location
+    elif type == 'mp4':
+        export_func = make_video
+        namespace = 'VIDEO:'
+        export_path = mp4_location
+    try:
+        start_t = datetime.now()
+        show_progress(
+            export_func, 
+            msg='{} converting'.format(namespace), 
+            sec=1
+        )
+        print '{} {}'.format(namespace, export_path)
+    except KeyboardInterrupt, SystemExit:
+        print '{} convertion stopped.'.format(namespace)
+    finally:
+        elapsed_s = abs((datetime.now() - start_t).seconds)
+        print '{}'.format(export_path)
+        print '{} completed in {} seconds'.format(namespace, elapsed_s)
+
+def finish_capture():
+    print 'Finishing capture!\n'
+
+    # Create an animated gif (Requires ImageMagick).
+    if config['create_gif']:
+        export_timelapse('gif')
+
+    # Create a video (Requires avconv - which is basically ffmpeg).
+    if config['create_video']:
+        export_timelapse('mp4')
+
+    # exit or schedule script
+    if mode['type'] == 'Once':
+        print 'Time-lapse capture complete!'
+        sys.exit()
+
+    elif mode['type'] == 'daily':
+
+        now = datetime.now()
+        # calculate time till tomorrows start
+        eod = datetime(now.year, now.month, now.day) + timedelta(days=1, microseconds=-1)
+        sod = eod + timedelta(microseconds=1)
+        start_tomorrow = ((eod - now)+(mode['start_time'] - sod)).seconds
+
+        print 'Capture complete for today. Starting tomorrow @ {}'.format(
+            mode['start_time'].strftime(time_string))
+
+        # schedule start_capture to run again at start_time
+        thread = threading.Timer(start_tomorrow, start_capture).start()
+
+
+def wait_or_capture_image():
+    # get time now
+    now = datetime.now()
+
+    # if before start time wait
+    if mode['type'] == 'daily' and now < mode['start_time']:
+        # schedule start_capture to run at start_time
+        delay = (mode['start_time'] - now).total_seconds()
+        if delay > 1:
+            print '\nWaiting till:', mode['start_time'].strftime(FMT), '\n'
+            thread = threading.Timer(delay, capture_image).start()
+        else:
+            sleep(delay)
+    # if after finish time wait
+    elif mode['type'] == 'daily' and now > mode['end_time']:
+        finish_capture()
+    # capture images
+    else:
+        print 'Starting capture!\n'
+        capture_image()
+
+
+def start_capture():
+    # config mode
+    config_mode()
+    # create the directory
+    create_timestamped_dir(mode['dir'])
+    # wait or capture images
+    wait_or_capture_image()
+
+
+# sets capture mode and starts capture
+start_capture()
